5af028ddf2d671222b22f1a677cd47c120ee4ef9
Bernardo Marambaia <marambaia.bma@gmail.com>
2022-06-02 07:42:04 -0300

    first commit

---
+++ src/main/java/br/com/qintess/Escala.java
@@ -0,0 +1,13 @@
+package br.com.qintess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Escala {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Escala.class, args);
+    }
+
+}
