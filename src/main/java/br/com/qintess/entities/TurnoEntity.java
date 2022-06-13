41c8fc0d9d47140a22d1fea8e6b3b4c7fe0f72d5
Raul Melo <raul_k-22@hotmail.com>
2022-06-13 17:25:01 -0300

    Atualizado o layout.

--- src/main/java/br/com/qintess/entities/TurnoEntity.java
+++ src/main/java/br/com/qintess/entities/TurnoEntity.java
@@ -1,104 +1,24 @@
 package br.com.qintess.entities;
 
 
-import java.time.LocalTime;
-import java.util.Objects;
-import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 
+@Entity(name = "TB_TURNO")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class TurnoEntity {
 
-  private UUID id;
-  private String descricao;
+  @Id
+  private Integer id;
+  private String nome;
   private String sigla;
-  private LocalTime horarioInicio;
-  private LocalTime horarioTermino;
-  private LocalTime totalHoras;
-
-  public TurnoEntity(UUID id, String descricao, String sigla, LocalTime horarioInicio, LocalTime horarioTermino, LocalTime totalHoras) {
-    this.id = id;
-    this.descricao = descricao;
-    this.sigla = sigla;
-    this.horarioInicio = horarioInicio;
-    this.horarioTermino = horarioTermino;
-    this.totalHoras = totalHoras;
-  }
-
-  public TurnoEntity() {
-  }
-
-  public UUID getId() {
-    return id;
-  }
-
-  public void setId(UUID id) {
-    this.id = id;
-  }
-
-  public String getDescricao() {
-    return descricao;
-  }
-
-  public void setDescricao(String descricao) {
-    this.descricao = descricao;
-  }
-
-  public String getSigla() {
-    return sigla;
-  }
-
-  public void setSigla(String sigla) {
-    this.sigla = sigla;
-  }
-
-  public LocalTime getHorarioInicio() {
-    return horarioInicio;
-  }
-
-  public void setHorarioInicio(LocalTime horarioInicio) {
-    this.horarioInicio = horarioInicio;
-  }
-
-  public LocalTime getHorarioTermino() {
-    return horarioTermino;
-  }
-
-  public void setHorarioTermino(LocalTime horarioTermino) {
-    this.horarioTermino = horarioTermino;
-  }
-
-  public LocalTime getTotalHoras() {
-    return totalHoras;
-  }
-
-  public void setTotalHoras(LocalTime totalHoras) {
-    this.totalHoras = totalHoras;
-  }
-
-  @Override
-  public String toString() {
-    return "TurnoEntity{" +
-      "id=" + id +
-      ", descricao='" + descricao + '\'' +
-      ", sigla='" + sigla + '\'' +
-      ", horarioInicio=" + horarioInicio +
-      ", horarioTermino=" + horarioTermino +
-      ", totalHoras=" + totalHoras +
-      '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    TurnoEntity that = (TurnoEntity) o;
-    return Objects.equals(id, that.id) && Objects.equals(descricao, that.descricao) && Objects.equals(sigla, that.sigla) && Objects.equals(horarioInicio, that.horarioInicio) && Objects.equals(horarioTermino, that.horarioTermino) && Objects.equals(totalHoras, that.totalHoras);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, descricao, sigla, horarioInicio, horarioTermino, totalHoras);
-  }
+  private boolean trabalhaNoFeriado;
+  private String horaIncial;
+  private String horaFinal;
+  private String totalHoras;
 
 
 }
