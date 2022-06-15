4a77b9c711cfa40b0d87d2765274ad4ccd2c5577
Raul Melo <raul_k-22@hotmail.com>
2022-06-15 13:07:09 -0300

    Adicionado a migration V016 para inserir dados na tabela TB_FERIADO.

---
+++ src/main/resources/db/migration/V016__add_dados_tabela_feriado.sql
@@ -0,0 +1,4 @@
+INSERT INTO TB_FERIADO(nome,data,tipo,dia_semana)VALUES('Dia do Trabalho','01/05/2022','Nacional','Domingo');
+INSERT INTO TB_FERIADO(nome,data,tipo,dia_semana)VALUES('Independência do Brasil','07/09/2022','Nacional','Quarta-feira');
+INSERT INTO TB_FERIADO(nome,data,tipo,dia_semana)VALUES('Revolução Pernambucana (Data Magna)','06/03/2022','Estadual','Domingo');
+INSERT INTO TB_FERIADO(nome,data,tipo,dia_semana)VALUES('São João','24/06/2022','Estadual','Sexta-feira');
