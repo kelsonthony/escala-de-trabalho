f44508c0f3ebacceaa342d9e96e353d51560fa34
Raul Melo <raul_k-22@hotmail.com>
2022-06-15 12:39:27 -0300

    Adicionado a migration V015 para incluir dados a tabela TB_CARGO_FUNCIONARIO .

---
+++ src/main/resources/db/migration/V015__add_dados_tabela_cargo_funcionario.sql
@@ -0,0 +1,4 @@
+INSERT INTO TB_CARGO_FUNCIONARIO(nome,sigla,permissao_acesso,horas_extras_permitidas) VALUES ('Apoio à Qualidade e Melhoria Contínua','AQMC','Usuário',20);
+INSERT INTO TB_CARGO_FUNCIONARIO(nome,sigla,permissao_acesso,horas_extras_permitidas) VALUES ('Atendimento de Solicitações','AS','Usuário',10);
+INSERT INTO TB_CARGO_FUNCIONARIO(nome,sigla,permissao_acesso,horas_extras_permitidas) VALUES ('Coordenador de Processos para o Suporte Técnico','CPST','Editor',33);
+INSERT INTO TB_CARGO_FUNCIONARIO(nome,sigla,permissao_acesso,horas_extras_permitidas) VALUES ('Coordenadores de Processos, devendo assumir a coordenação de todos os processos','CPCP','Admin',50);
