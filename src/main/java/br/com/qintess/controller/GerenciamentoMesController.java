4b40b688a45df62f9331e293a234444473bb2000
Raul Melo <raul_k-22@hotmail.com>
2022-07-04 10:17:44 -0300

    Adicionado o controller de gerenciamento mes e atualizado a classe GerenciamentoMesFixo.

---
+++ src/main/java/br/com/qintess/controller/GerenciamentoMesController.java
@@ -0,0 +1,39 @@
+package br.com.qintess.controller;
+
+import br.com.qintess.entities.Escala;
+import br.com.qintess.entities.Funcionario;
+import br.com.qintess.services.interfaces.IEscalaService;
+import br.com.qintess.services.interfaces.IFuncionarioService;
+import br.com.qintess.services.interfaces.IGerenciamentoMesFixoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("gerenciamento")
+public class GerenciamentoMesController {
+
+  @Autowired
+  private IFuncionarioService funcionarioService;
+  @Autowired
+  private IEscalaService escalaService;
+  @Autowired
+  private IGerenciamentoMesFixoService gerenciamentoMesFixoService;
+
+  @GetMapping("/cadastrar")
+  public ModelAndView listar(ModelMap model) {
+
+    Funcionario funcionario = this.funcionarioService.listarPorId(2);
+    Escala escala = this.escalaService.listarPorId(2);
+    this.gerenciamentoMesFixoService.cadastrarMes(funcionario,escala);
+
+    return new ModelAndView("/escala/list", model);
+
+  }
+
+
+
+}
