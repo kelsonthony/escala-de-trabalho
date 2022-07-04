e4c22ac8e51020dd79de7a9da1c417486bafbfba
Bernardo Marambaia <marambaia.bma@gmail.com>
2022-07-04 18:33:17 -0300

    Atualizada a funcionalidade de Cargos.

--- src/main/java/br/com/qintess/entities/Cargo.java
+++ src/main/java/br/com/qintess/entities/Cargo.java
@@ -4,6 +4,7 @@ import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 import java.util.Objects;
@@ -17,22 +18,18 @@ public class Cargo {
     @Column(name = "ID")
     private long cargoId;
 
-    @Size(min = 2, max = 125, message = "Tamanho deve estar entre 2 e 125")
+    @Size(min = 2, max = 125, message = "O tamanho do nome deve ser entre 2 e 125 caracteres")
     @Column(nullable = false, length = 125)
     private String nome;
 
-    @Size(min = 2, max = 10, message = "Tamanho deve estar entre 2 e 10")
+    @Size(min = 2, max = 10, message = "O tamanho da sigla deve ser entre 2 e 10 caracteres")
     @Column(nullable = false, length = 10)
     private String sigla;
 
-    @Size(min = 2, max = 10, message = "Tamanho deve estar entre 2 e 10")
-    @Column(nullable = false, length = 10)
-    private String permissaoAcesso;
-
-    @NotBlank
+    @NotNull(message = "Digite o número de Horas Extras Permitidas ou 0 para nenhuma")
     @Range(min = 0, max = 999)
-    @Column(nullable = true, length = 999)
-    private int horasExtrasPermitidas = 0;
+    @Column(length = 999)
+    private Integer horasExtrasPermitidas;
 
     @OneToMany(mappedBy = "cargo")
     private List<Funcionario> funcionarios;
@@ -61,20 +58,12 @@ public class Cargo {
         this.sigla = sigla;
     }
 
-    public String getPermissaoAcesso() {
-        return permissaoAcesso;
-    }
-
-    public void setPermissaoAcesso(String permissaoAcesso) {
-        this.permissaoAcesso = permissaoAcesso;
-    }
-
-    public int getHorasExtrasPermitidas() {
+    public Integer getHorasExtrasPermitidas() {
         return horasExtrasPermitidas;
     }
 
-    public void setHorasExtrasPermitidas(int horasExtrasPermitidas) {
-        this.horasExtrasPermitidas = horasExtrasPermitidas;
+    public void setHorasExtrasPermitidas(Integer horasExtrasPermitidas) {
+            this.horasExtrasPermitidas = horasExtrasPermitidas;
     }
 
     public List<Funcionario> getFuncionarios() {
