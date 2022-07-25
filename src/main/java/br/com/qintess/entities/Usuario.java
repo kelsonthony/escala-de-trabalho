1fbc59f3615af323069f8014c26c3e3b73505add
Bernardo Albuquerque <bernardo.albuquerque@qintess.com>
2022-07-25 18:08:23 -0300

    Adicionada a entidade Usuario para mapear os dados de TB_USUARIO e implementa a interface UserDetails.

---
+++ src/main/java/br/com/qintess/entities/Usuario.java
@@ -0,0 +1,132 @@
+package br.com.qintess.entities;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "TB_USUARIO")
+public class Usuario implements UserDetails {
+
+    /**
+     * Serial Version UID
+     */
+    private static final long serialVersionUID = -7485092339472089435L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long usuarioId;
+
+    @Column(name = "LOGIN", unique=true, length = 125, nullable = false)
+    private String login;
+
+    @Column(name = "SENHA", length = 255, nullable = false)
+    private String senha;
+
+    @Column(name = "NOME", length = 125, nullable = false)
+    private String nome;
+
+    @Column(name = "SOBRENOME", length = 255, nullable = false)
+    private String sobrenome;
+
+    @ManyToMany
+    @JoinTable(name = "TB_PERFIL_USUARIO",
+            joinColumns = { @JoinColumn(name = "USUARIO_ID") },
+            inverseJoinColumns = { @JoinColumn(name = "PERFIL_ID") })
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<Perfil> perfis = new ArrayList<Perfil>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Autorizacao> autorizacoes = new HashSet<Autorizacao>();
+        if(perfis != null) {
+            for(Perfil perfil : perfis) {
+                autorizacoes.addAll(perfil.getAutorizacoes());
+            }
+        }
+        return autorizacoes;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Long getId() {
+        return usuarioId;
+    }
+
+    public void setId(Long id) {
+        this.usuarioId = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public List<Perfil> getPerfis() {
+        return perfis;
+    }
+
+    public void setPerfis(List<Perfil> perfis) {
+        this.perfis = perfis;
+    }
+}
