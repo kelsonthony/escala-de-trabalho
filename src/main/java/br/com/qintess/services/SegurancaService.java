221406dfadc5652da528e2ac0f9002785316aa20
Bernardo Albuquerque <marambaia.bma@gmail.com>
2022-07-25 19:22:55 -0300

    Adicionado o service SegurancaService para gerenciar as actions de segurança do sistema.

---
+++ src/main/java/br/com/qintess/services/SegurancaService.java
@@ -0,0 +1,29 @@
+package br.com.qintess.services;
+
+import br.com.qintess.entities.Usuario;
+import br.com.qintess.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("segurancaService")
+public class SegurancaService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public void setUsuarioRepository(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String nome) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByNome(nome);
+        if (usuario.equals(null))
+            throw new UsernameNotFoundException("Usuário não encontrado!");
+
+        return usuario;
+    }
+}
