4dbca2f22cae33e7e09ec8d8ba7a233a50713ad5
Raul Melo <raul_k-22@hotmail.com>
2022-07-27 08:07:00 -0300

    Adicionado o template de listar perfil.

--- src/main/java/br/com/qintess/controller/PerfilController.java
+++ src/main/java/br/com/qintess/controller/PerfilController.java
@@ -1,23 +1,57 @@
 package br.com.qintess.controller;
 
+import br.com.qintess.entities.Autorizacao;
+import br.com.qintess.entities.Perfil;
+import br.com.qintess.services.interfaces.IAutorizacaoService;
+import br.com.qintess.services.interfaces.IPerfilService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("perfil")
 public class PerfilController {
 
-//    @Autowired
-//    private IPerfilService perfilService;
+    @Autowired
+    private IPerfilService perfilService;
+
+    @Autowired
+    private IAutorizacaoService autorizacaoService;
 
     @GetMapping("/listar")
     public ModelAndView listar(ModelMap model) {
-//        List<Perfil> perfis = perfilService.listar();
 
-//        model.addAttribute("perfis", perfis);
-        return new ModelAndView("/usuario/list", model);
+      List<Perfil> perfis = perfilService.listar();
+      model.addAttribute("perfis", perfis);
+      return new ModelAndView("/perfil/list", model);
     }
+
+    @GetMapping("{id}/atualizar")
+    public ModelAndView atualizar(@PathVariable("id") Long id){
+
+      ModelAndView mv = new ModelAndView("/perfil/update");
+
+      Optional<Perfil> perfilCadastrado = this.perfilService.listarPorId(id);
+      List<Autorizacao> autorizacaoList = this.autorizacaoService.listar();
+
+      if(perfilCadastrado.isPresent()){
+        mv.addObject("perfil",perfilCadastrado.get());
+        mv.addObject("autorizacoesList",autorizacaoList);
+        return mv;
+      }else{
+        System.out.println("################ NÂO ENCONTRADO ###############################");
+      }
+       return mv;
+    }
+
+
+
+
 }
