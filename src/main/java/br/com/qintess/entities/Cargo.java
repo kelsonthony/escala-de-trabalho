4a501bf129cc7bc8f994f0e997a55b7417f6c325
Raul Melo <raul_k-22@hotmail.com>
2022-09-02 11:05:21 -0300

    Atualizado a entidade ( Cargo ) e seu template de cadastro ( add.html ),  para adicionar os novos atributos (cargaHorariaDia e cargaHorariaMês).

--- src/main/java/br/com/qintess/entities/Cargo.java
+++ src/main/java/br/com/qintess/entities/Cargo.java
@@ -13,64 +13,101 @@ import java.util.Objects;
 @Table(name = "TB_CARGO")
 public class Cargo {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID")
-    private long cargoId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "ID")
+  private long cargoId;
 
-    @Size(min = 2, max = 125, message = "O tamanho do nome deve ser entre 2 e 125 caracteres")
-    @Column(nullable = false, length = 125)
-    private String nome;
+  @Size(min = 2, max = 125, message = "O tamanho do nome deve ser entre 2 e 125 caracteres")
+  @Column(nullable = false, length = 125)
+  private String nome;
 
-    @Size(min = 2, max = 10, message = "O tamanho da sigla deve ser entre 2 e 10 caracteres")
-    @Column(nullable = false, length = 10)
-    private String sigla;
+  @Size(min = 2, max = 10, message = "O tamanho da sigla deve ser entre 2 e 10 caracteres")
+  @Column(nullable = false, length = 10)
+  private String sigla;
 
-    @NotNull(message = "Digite o número de Horas Extras Permitidas ou 0 para nenhuma")
-    @Range(min = 0, max = 999)
-    @Column(length = 999)
-    private Integer horasExtrasPermitidas;
+  @Column(name = "carga_horaria_dia", nullable = false)
+  private float cargaHorariaDia;
 
-    @OneToMany(mappedBy = "cargo")
-    private List<Funcionario> funcionarios;
+  @Column(name = "carga_horaria_mes")
+  private float cargaHorariaMes;
 
-    public long getCargoId() {
-        return cargoId;
-    }
+  @NotNull(message = "Digite o número de Horas Extras Permitidas ou 0 para nenhuma")
+  @Range(min = 0, max = 999)
+  @Column(length = 999)
+  private Integer horasExtrasPermitidas;
 
-    public void setCargoId(long cargoId) {
-        this.cargoId = cargoId;
-    }
+  @OneToMany(mappedBy = "cargo")
+  private List<Funcionario> funcionarios;
 
-    public String getNome() {
-        return nome;
-    }
+  public Cargo() {
+  }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+  public Cargo(long cargoId, String nome, String sigla, float cargaHorariaDia, float cargaHorariaMes, Integer horasExtrasPermitidas, List<Funcionario> funcionarios) {
+    this.cargoId = cargoId;
+    this.nome = nome;
+    this.sigla = sigla;
+    this.cargaHorariaDia = cargaHorariaDia;
+    this.cargaHorariaMes = cargaHorariaMes;
+    this.horasExtrasPermitidas = horasExtrasPermitidas;
+    this.funcionarios = funcionarios;
+  }
 
-    public String getSigla() {
-        return sigla;
-    }
+  public long getCargoId() {
+    return cargoId;
+  }
 
-    public void setSigla(String sigla) {
-        this.sigla = sigla;
-    }
+  public void setCargoId(long cargoId) {
+    this.cargoId = cargoId;
+  }
 
-    public Integer getHorasExtrasPermitidas() {
-        return horasExtrasPermitidas;
-    }
+  public String getNome() {
+    return nome;
+  }
 
-    public void setHorasExtrasPermitidas(Integer horasExtrasPermitidas) {
-            this.horasExtrasPermitidas = horasExtrasPermitidas;
-    }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
-    public List<Funcionario> getFuncionarios() {
-        return funcionarios;
-    }
+  public String getSigla() {
+    return sigla;
+  }
 
-    public void setFuncionarios(List<Funcionario> funcionarios) {
-        this.funcionarios = funcionarios;
-    }
+  public void setSigla(String sigla) {
+    this.sigla = sigla;
+  }
+
+  public float getCargaHorariaDia() {
+    return cargaHorariaDia;
+  }
+
+  public void setCargaHorariaDia(float cargaHorariaDia) {
+    this.cargaHorariaDia = cargaHorariaDia;
+  }
+
+  public float getCargaHorariaMes() {
+    return cargaHorariaMes;
+  }
+
+  public void setCargaHorariaMes(float cargaHorariaMes) {
+    this.cargaHorariaMes = cargaHorariaMes;
+  }
+
+  public Integer getHorasExtrasPermitidas() {
+    return horasExtrasPermitidas;
+  }
+
+  public void setHorasExtrasPermitidas(Integer horasExtrasPermitidas) {
+    this.horasExtrasPermitidas = horasExtrasPermitidas;
+  }
+
+  public List<Funcionario> getFuncionarios() {
+    return funcionarios;
+  }
+
+  public void setFuncionarios(List<Funcionario> funcionarios) {
+    this.funcionarios = funcionarios;
+  }
+
+
 }
