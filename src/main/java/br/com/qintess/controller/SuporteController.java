9c5d460d3ea6abdd76dcead131d38e4833201711
Bernardo Albuquerque <marambaia.bma@gmail.com>
2022-09-05 16:47:04 -0300

    Funcionalidades de suporte ao sistema.

--- src/main/java/br/com/qintess/controller/SuporteController.java
+++ src/main/java/br/com/qintess/controller/SuporteController.java
@@ -2,19 +2,18 @@ package br.com.qintess.controller;
 
 import br.com.qintess.entities.Suporte;
 import br.com.qintess.services.interfaces.ISuporteService;
+import com.sun.jndi.url.corbaname.corbanameURLContextFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("suporte")
@@ -23,12 +22,19 @@ public class SuporteController {
     @Autowired
     private ISuporteService suporteService;
 
+    @ResponseBody
     @GetMapping("/listar")
-    public ModelAndView listar(ModelMap model) {
+    public ModelAndView listar(@RequestParam(name = "rt") String rt,
+                               @RequestParam(name = "st") String st,
+                               ModelMap model) {
+        // rt - Request type
+        // st - Status type
+        List<Suporte> chamados = null;
+        if ((rt == null || rt.isEmpty() || rt == "T") && (st == null || st.isEmpty() || rt == "T"))
+            chamados = this.suporteService.listar();
 
-        model.addAttribute("mensagens",this.suporteService.listar());
+        model.addAttribute("chamados", chamados);
         return new ModelAndView("suporte/list", model);
-
     }
 
     @GetMapping("/cadastrar")
